d7c4596dc041855615af0e1ed4b39e5c47eb81e1
Mahendra <mahendramistri@gmail.com>
2024-04-28 22:02:08 +0530

    first commit.

---
+++ src/test/java/rahulshettyacademy/TestNGTest.java
@@ -0,0 +1,11 @@
+package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class TestNGTest {
+    @Test
+    public void car_loan()
+    {
+        System.out.println("Car Loan");
+    }
+}
